ad741ff2fc8aaf8ca645a375315e0aa82f3774db
moca <t_sato@yumemi.co.jp>
2018-10-16 22:37:13 +0900

    微調整

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by 佐藤 節夏 on 2018/07/27.
-//  Copyright © 2018年 mcxmc. All rights reserved.
-//
-
 import UIKit
 import RealmSwift
 import UserNotifications
